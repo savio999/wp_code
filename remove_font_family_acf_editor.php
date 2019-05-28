262fc7ae7da0f2131279b44f0f8b71479744dd8b
Savio Menezes <savio.menezes@kodework.com>
2019-05-28 10:17:19 +0530

    remove font family from acf editor

---
+++ remove_font_family_acf_editor.php
@@ -0,0 +1,6 @@
+<?php
+// add font family select in editor
+function acf_wysiwyg_remove_wpautop() {
+    remove_filter('acf_the_content', 'wpautop' );
+}
+add_action('acf/init', 'acf_wysiwyg_remove_wpautop', 15);
